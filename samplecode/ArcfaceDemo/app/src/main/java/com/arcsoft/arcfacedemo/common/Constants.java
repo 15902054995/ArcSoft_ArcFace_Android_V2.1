61ab6de7e0ca38d05e37d135ccbb595b7d6f6322
Lyman <1904362406@qq.com>
2019-05-27 22:18:15 +0800

    注释语句

---
+++ samplecode/ArcfaceDemo/app/src/main/java/com/arcsoft/arcfacedemo/common/Constants.java
@@ -0,0 +1,6 @@
+package com.arcsoft.arcfacedemo.common;
+
+public class Constants {
+    public static final String APP_ID = "DSTKc83T6PEfuGoWb6KBvjZ5XWo4AMf8q8rCu3nGM1C9";
+    public static final String SDK_KEY = "FFuF4WYuUXHU15RLnbQPckJ53mMBR96mDP4ov8s4JUXy";
+}
